a468c8246c914a159fa27ccdb89938a7efb88515
AjaiVeer Singh <ajaiveersandhu@gmail.com>
2020-01-15 19:36:15 -0500

    InClass 2 Part 1 & Part 2 completed

---
+++ Assignments/InClass2/InClass-Part1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyNameApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Name : ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Your Name is {0}.", userName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignments/InClass2/InClass-Part2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyNameApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Name : ");
+            string userName = Console.ReadLine();
+
+            int userAge;
+            do
+            {
+                Console.Write("Please Enter Your Age : ");
+                userAge = Convert.ToInt32(Console.ReadLine());
+
+                if(userAge < 0)
+                {
+                    Console.WriteLine("Your age can't be less than 0(zero).");
+                }
+            } while (userAge < 0);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("\tYour Name is {0}.\n\tYour age is {1}", userName, userAge);
+            Console.ReadLine();
+        }
+    }
+}
