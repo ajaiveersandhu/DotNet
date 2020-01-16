99e504f4af4a45d1316772c844fec3e7741ec8a5
AjaiVeer Singh <ajaiveersandhu@gmail.com>
2020-01-15 19:35:27 -0500

    Files renamed for more organization.

---
+++ Assignments/InClass1/InClass1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyHelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is AjaiVeer.");
+            Console.Title = "InClass1";
+            Console.ReadLine();
+        }
+    }
+}
